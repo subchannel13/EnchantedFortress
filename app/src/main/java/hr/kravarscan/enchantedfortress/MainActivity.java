1010432f7f34e357cd06b94be1c0bcb14af7f7b2
Ivan Kravarscan <subchannel13@gmail.com>
2017-05-24 22:13:25 +0200

    Main menu and stub game fragment

--- app/src/main/java/hr/kravarscan/enchantedfortress/MainActivity.java
+++ app/src/main/java/hr/kravarscan/enchantedfortress/MainActivity.java
@@ -1,13 +1,23 @@
 package hr.kravarscan.enchantedfortress;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.os.Debug;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements MainMenuFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainMenuFragment mainMenu = new MainMenuFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mainMenu).commit();
+    }
+
+    @Override
+    public void onNewGame() {
+        Log.i("EF main", "new game");
     }
 }
---
+++ app/src/main/java/hr/kravarscan/enchantedfortress/MainMenuFragment.java
@@ -0,0 +1,55 @@
+package hr.kravarscan.enchantedfortress;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainMenuFragment extends Fragment {
+
+    private OnFragmentInteractionListener listener;
+
+    public MainMenuFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+        layout.findViewById(R.id.newGameButton).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        listener.onNewGame();
+                    }
+                }
+        );
+
+        return layout;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            listener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onNewGame();
+    }
+}
