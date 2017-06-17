3f3583d745c8a319fcd0906a6e87776449ad4beb
Ivan Kravarscan <subchannel13@gmail.com>
2017-06-17 14:12:33 +0200

    Refactored "about" screen

--- app/src/main/java/hr/kravarscan/enchantedfortress/AboutFragment.java
+++ app/src/main/java/hr/kravarscan/enchantedfortress/AboutFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class AboutFragment extends Fragment {
@@ -19,8 +20,37 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        ((TextView)view.findViewById(R.id.aboutDeveloper)).setText(
+                getResources().getString(R.string.aboutDevelopBy) + " Ivan Kravarščan"
+        );
+
+        TextView translators = (TextView) view.findViewById(R.id.aboutTranslator);
+        if (getResources().getStringArray(R.array.translators).length > 0) {
+            translators.setText(
+                    getResources().getString(R.string.aboutTranslateBy) + " "
+            );
+        }
+        else
+            translators.setVisibility(View.GONE);
+
+        ((TextView)view.findViewById(R.id.aboutTracker)).setText(
+                getResources().getString(R.string.aboutTrackerAt) + "\nhttps://github.com/subchannel13/EnchantedFortress/issues"
+        );
+
+        ((TextView)view.findViewById(R.id.aboutCode)).setText(
+                getResources().getString(R.string.aboutCodeAt) + "\nhttps://github.com/subchannel13/EnchantedFortress"
+        );
+
+        ((TextView)view.findViewById(R.id.aboutSupport)).setText(
+                getResources().getString(R.string.aboutSupportAt) + "\nhttps://www.paypal.me/IvanKravarscan/5"
+        );
+
+        ((TextView)view.findViewById(R.id.aboutVersion)).setText(
+                getResources().getString(R.string.aboutVersion) + ": " + BuildConfig.VERSION_NAME
+        );
+        return view;
     }
 
 }
