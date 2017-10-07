d82f2ff65992f2cda26ce998a79ff5ddeced70c1
Ivan Kravarscan <subchannel13@gmail.com>
2017-10-07 21:06:00 +0200

    Multiple scores show properly

--- app/src/main/java/hr/kravarscan/enchantedfortress/ScoresFragment.java
+++ app/src/main/java/hr/kravarscan/enchantedfortress/ScoresFragment.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.TextView;
 
-import hr.kravarscan.enchantedfortress.UiUtils.ScoresAdapter;
 import hr.kravarscan.enchantedfortress.storage.HighScores;
+import hr.kravarscan.enchantedfortress.storage.ScoreEntry;
 
 
 public class ScoresFragment extends AAttachableFragment {
@@ -22,8 +22,17 @@ public class ScoresFragment extends AAttachableFragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scores, container, false);
 
-        ListView listView = view.findViewById(R.id.scoreList);
-        listView.setAdapter(new ScoresAdapter(this.getActivity(), HighScores.get().getAll()));
+        CharSequence[] difficultyNames = this.getActivity().getResources().getTextArray(R.array.difficultyLevels);
+        ViewGroup listView = view.findViewById(R.id.scoreList);
+
+        for (ScoreEntry score : HighScores.get().getAll()) {
+            View itemView = LayoutInflater.from(this.getActivity()).inflate(R.layout.score_entry_view, listView, false);
+
+            ((TextView)itemView.findViewById(R.id.turnsText)).setText(Integer.toString(score.getTurn()));
+            ((TextView)itemView.findViewById(R.id.difficultyText)).setText(difficultyNames[score.getDifficulty()]);
+
+            listView.addView(itemView);
+        }
 
         return view;
     }
